bf7c69b58049cdb34fd72aaff3d3e847a29a0d59
jinisha2193@gmail.com <jinisha2193@gmail.com>
2022-10-08 23:06:02 -0500

    Added todo ui

--- apps/todos/src/app/app.tsx
+++ apps/todos/src/app/app.tsx
@@ -1,14 +1,59 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
+import { useState } from 'react';
 import styles from './app.module.css';
-import NxWelcome from './nx-welcome';
 
-export function App() {
-  return (
-    <>
-      <NxWelcome title="todos" />
-      <div />
-    </>
-  );
+interface Todo {
+  title: string;
+  status: boolean;
 }
 
+const defaultTodo: Todo = {
+  title: 'new Todo',
+  status: false,
+};
+
+export const App = () => {
+  const [todos, setTodos] = useState<Todo[]>([
+    { title: 'Todo List item 1', status: true },
+    { title: 'Todo List item 2', status: false },
+  ]);
+
+  const updateTodoStatus = (todo: Todo): void => {
+    const indexOfTodoToBeUpdated = todos.findIndex((eachTodo) => {
+      return eachTodo.title === todo.title;
+    });
+    const newArray = [...todos];
+    newArray[indexOfTodoToBeUpdated].status = !todo.status;
+    setTodos(newArray);
+  };
+  const addToDO = (todo: Todo): void => {
+    setTodos((existingToDo) => [...existingToDo, todo]);
+  };
+  return (
+    <>
+      <h1>Todos</h1>
+      <ul>
+        {todos.map((t) => (
+          <li className={'todo'} key={t.title}>
+            <input
+              type="checkbox"
+              id={t.title}
+              checked={t.status}
+              onChange={() => updateTodoStatus(t)}
+            />
+            {t.title}
+          </li>
+        ))}
+      </ul>
+      <button
+        id={'add-todo'}
+        onClick={() => {
+          addToDO(defaultTodo);
+        }}
+      >
+        Add Todo
+      </button>
+    </>
+  );
+};
 export default App;
